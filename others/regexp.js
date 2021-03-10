98590c9b0a6a8f8f10919835aa014fbb5f04279d
jiangliangliang <jiangliangliang@starlinke.com>
2021-03-10 15:43:51 +0800

    util - moneyFormatter 金额按每千分位逗号分隔

--- others/regexp.js
+++ others/regexp.js
@@ -1,18 +1,5 @@
-// var string = "12345678"
-// var reg = /(?!^)(?=(\d{3})+$)/g;
-// var result = string.replace(reg, ',');
-// console.log(result);
-
-// const regexp = new RegExp(/(?!^)(?=(\d{3})+$)/, 'g')
-// console.log(regexp)
-// const result = regexp.exec(string)
-// console.log(result)
-
 // const reg1 = new RegExp(/(\d)(?=(?:\d{3})+$)/, 'g')
-
-// console.log(reg1.exec(str1))
-// const result1 = str1.replace(reg1, '$1,')
-// console.log(result1)
+// const result1 = '12354520'.replace(reg1, '$1,')
 
 function moneyFormatter(num) {
     num = +num
